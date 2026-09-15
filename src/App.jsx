131f9dbf45013c25a794715057031ac27f9ce853
shushmitatiwari <shushmita.tiwari@powerup.money>
2026-09-15 13:17:00 +0530

    fixed  links

--- src/App.jsx
+++ src/App.jsx
@@ -121,7 +121,7 @@ function App() {
         <a href="https://github.com/Shushmitaaaa" target="_blank" title="GitHub"><Github size={20} strokeWidth={2.2} /></a>
         <a href="https://www.linkedin.com/in/shushmita-tiwari-811906273/" target="_blank" title="LinkedIn"><Linkedin size={20} strokeWidth={2.2} /></a>
         <a href="https://x.com/Shushmitaaa?s=20" target="_blank" title="X (Twitter)"><Twitter size={20} strokeWidth={2.2} /></a>
-        <a href="https://docs.google.com/document/d/1MWaKA1RaWKKTLcQnjEWDrYZXd7VImLuaqXgLZzmM0O8/edit?usp=drive_link" target="_blank" title="Resume"><FileText size={20} strokeWidth={2.2} /></a>
+        <a href="resume_shushmita.pdf" target="_blank" title="Resume"><FileText size={20} strokeWidth={2.2} /></a>
 
       </nav>
     </div>
